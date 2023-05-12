5d8f4fd14b3e4cb7009f433d6c19ff2168b719cd
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-12 07:11:55 +0000

    [Save]5/12/16:11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
-    user_path(@user.id)
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
