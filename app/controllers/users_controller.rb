11cc77a768fc22406c336acd6e39119770b7d59e
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-12 03:15:28 +0000

    [Save]5/12/12:15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.find(current_user.id)
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Welcome! You have signed up successfully."
+        redirect_to user_path(@user.id)
+      else
+        render :new
+      end
+  end
 
   def show
     @user = User.find(params[:id])
