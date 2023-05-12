62e81cb58421487e59516d2380ce211c38f19a62
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-12 06:18:33 +0000

    [Save]5/12/15:18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.find(current_user.id)
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
       if @user.save
