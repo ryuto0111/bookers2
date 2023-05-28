3b50dd0cd3165bf5b4cdffa05f6a525603ea39ef
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-28 03:50:17 +0000

    [Add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/home/about" => "homes#about", as: "about"
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
 end
